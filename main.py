3fb3c8b8565905ca46a4d26146dd26780cbc4bfb
Meghan13 <meghangraham13@gmail.com>
2020-10-21 16:47:00 -0700

    Adding Display Screen with quit button.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+import pygame
+
+#Intialize the pygame
+pygame.init()
+
+#Create the screen
+screen = pygame.display.set_mode((800,600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
