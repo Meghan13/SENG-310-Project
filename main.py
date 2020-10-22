1725ef7f495d90252854f5494fa0068ff07125e4
madex <madexio@gmail.com>
2020-10-21 17:18:24 -0700

    Added a rectangle

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 running = True
+rect1 = pygame.Rect((100, 100),(100, 100))
+
+
+
 while running:
+    pygame.draw.rect(screen, pygame.Color(100, 100, 100), rect1)
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+
             running = False
