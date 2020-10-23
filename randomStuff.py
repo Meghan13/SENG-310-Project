20c3f3351098f26f362447d6bad12c33baec4e6d
madex <madexio@gmail.com>
2020-10-22 17:29:42 -0700

    Added random thing file

--- randomStuff.py
+++ randomStuff.py
@@ -1,8 +1,38 @@
 import pygame
 
+WIDTH = 800
+HEIGHT = 600
 
-def hello_world():
-    item = [("name",int)]
-    item.append(("Ben M", 10))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def hello_world(sort_option):
+    item = [("Matthew", 5, pygame.Rect((10, 10), (10, 10))), ("Andrew", 53, pygame.Rect((30, 10), (10, 10))),
+            ("Ben K", 10, pygame.Rect((50, 10), (10, 10))), ("Meghan", 22, pygame.Rect((50, 10), (10, 10))),
+            ("Ben M", 543216, pygame.Rect((50, 10), (10, 10)))]
+    if sort_option == 0:
+        item.sort(key=byName)
+    if sort_option == 1:
+        item.sort(reverse=True, key=byName)
+    if sort_option == 2:
+        item.sort(key=byNum)
+    if sort_option == 2:
+        item.sort(reverse=True, key=byNum)
+
+    rect_coords = (10, 10)
     for i in item:
         print(i)
+
+
+def byName(e):
+    return e[0]
+
+
+def byNum(e):
+    return e[1]
+
+
+hello_world(0)
+hello_world(1)
+hello_world(2)
+hello_world(3)
