376cb7342cded272590da7e332ab35f009dfe8bd
Andrew Cunningham <andrewthecunningham@gmail.com>
2020-10-27 01:28:55 -0700

    Added Inventory class, debugged Item class

--- testing.py
+++ testing.py
@@ -2,5 +2,8 @@
 
 name_list = ["Meghan", "Ben", "Andrew"]
 
+
 for name in name_list:
     print(f'Team names: {name}')
+
+# player_inventory = inv.Inventory(49, (400, 10), 7, pygame.Color(0, 64, 0), pygame.Color(0, 128, 0))
